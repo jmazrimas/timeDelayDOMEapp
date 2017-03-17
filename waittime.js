5e3096b7796cc62a037bec5bda7c51ac5bd29e1f
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-03-17 08:55:13 -0500

    basic node app written to take input time and write three outputs to dome after the specified delay

---
+++ waittime.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var domeInputs = {}
+
+var readDOMEInputs = function() {
+  // Read in variables from DOME
+  inData = fs.readFileSync('in.txt', 'utf8');
+  inputs = inData.split("\n");
+  for(var i=0; i<inputs.length; i++){
+    line = inputs[i];
+    nameVal = line.split("=");
+
+    if(nameVal.length > 1){
+      varName = nameVal[0].trim();
+      varValue = nameVal[1].trim();
+      domeInputs[varName] = varValue;
+    }
+
+  }
+}
+
+readDOMEInputs();
+
+fs.writeFile('out.txt', '', 'utf8', function(err) {
+  if(err) return console.log(err);
+});
+
+var delayTime = (domeInputs['timeToWait']/3)*1000
+var i = 1
+
+function writeOutput(){
+  var outputData = 'Interval'+i+' = '+parseInt(delayTime*i)+'ms\n'
+
+  fs.appendFile('out.txt', outputData, function (err) {
+    if(err) return console.log(err);
+  });
+
+  i++
+
+  if (i<4){
+    setTimeout(writeOutput, delayTime)
+  }
+
+}
+
+setTimeout(writeOutput, delayTime)
